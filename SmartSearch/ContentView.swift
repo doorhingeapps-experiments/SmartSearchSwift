da5aca1afdcac64ee16ae7805dc8187a1f895d63
Caedmon Myers <caedmonmyers@icloud.com>
2025-06-29 00:49:32 -0500

    Initial Commit

---
+++ SmartSearch/ContentView.swift
@@ -0,0 +1,27 @@
+//
+// SmartSearch
+// ContentView.swift
+//
+// Created on 6/29/25
+//
+// Copyright ©2025 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartSearch/SmartSearchApp.swift
@@ -0,0 +1,20 @@
+//
+// SmartSearch
+// SmartSearchApp.swift
+//
+// Created on 6/29/25
+//
+// Copyright ©2025 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+@main
+struct SmartSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
